1482c6ef47e127cc02ae7e69e4e78eb23b16e1b9
Aravinthan333 <aravinthan2606@gmail.com>
2024-08-26 10:46:59 +0530

    Partially Completed

---
+++ server/routes/generatorRoutes.js
@@ -0,0 +1,59 @@
+import express from "express";
+import * as dotenv from "dotenv";
+// import { Configuration, OpenAIApi } from "openai";
+import { OpenAI } from "openai";
+
+dotenv.config();
+
+const router = express.Router();
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// const openai = new OpenAIApi(configuration);
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from Image Generator!" });
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    // const aiResponse = await openai.createImage({
+    //   prompt,
+    //   n: 1,
+    //   size: "1024x1024",
+    //   response_format: "b64_json",
+    // });
+
+    const aiResponse = await openai.images.generate({
+      model: "dall-e-3",
+      prompt,
+      n: 1,
+      quality: "standard",
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    console.log(
+      "AAAAAAAAAIIIIIIIIIIIIIIII RESPONSEEEE  ========= ======== ========",
+      aiResponse
+    );
+
+    const image = aiResponse.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error("EEEERRRROOOORRRR====/n", error);
+    res
+      .status(500)
+      .send(error?.response?.error.message || "Something went wrong");
+  }
+});
+
+export default router;
